40db749fab4f4deca240d3d46a92fb2889104d01
Samuel Nicholas <nicholas.samuel@gmail.com>
2025-08-29 14:19:27 +0930

    Updated gitignore and deleted some duplicate files

--- project/test_main.gd
+++ project/test_main.gd
@@ -1,1 +1,11 @@
-extends "res://addons/enetheru.mp_test/main_scene.gd"
+extends "res://addons/enetheru.mp_test/base-runner.gd"
+
+func _init() -> void:
+	test_title = "State Generation"
+	test_desc = "Figuring out the idea I have for generating the majority of" \
+		+ "the information needed for testing, and providing tools to fill out" \
+		+ " the rest."
+
+
+func _setup() -> void:
+	pass
